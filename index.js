121b854425108460b3072d3c83c87668544c1466
MySeR <66944221+my5er@users.noreply.github.com>
2021-12-13 10:41:19 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const themes = {
+  default: {
+    title_color: "2f80ed",
+    icon_color: "4c71f2",
+    text_color: "333",
+    bg_color: "fffefe",
+    border_color: "e4e2e2",
+  },
+
+  dark: {
+    title_color: "fff",
+    icon_color: "79ff97",
+    text_color: "9f9f9f",
+    bg_color: "151515",
+  },
+  radical: {
+    title_color: "fe428e",
+    icon_color: "f8d847",
+    text_color: "a9fef7",
+    bg_color: "141321",
+  }
+};
+
+module.exports = themes;
